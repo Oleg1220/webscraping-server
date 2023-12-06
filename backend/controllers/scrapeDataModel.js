e47868558a22ed121a874310137df38a22e547f2
Gelo1220 <geloerni12@gmail.com>
2023-12-06 22:17:41 +0800

    Updated Server

--- backend/controllers/scrapeDataModel.js
+++ backend/controllers/scrapeDataModel.js
@@ -5,77 +5,96 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const TestUrl = "https://jmish.com/browse/";
 
-// @desc Get goals
-// @route GET /api/goals
+// @desc Get 
+// @route GET
 // @access Private
 const getData = asyncHandler(async (req, res) => {
+
+  const scraped_products  = []
+
+
+  const MainFunction = async (callback) => {
+      const urls = await getUrls();
   
-  const SrapeData = async () => {
-    const urls = await getUrls();
-    for (const url of urls) {
-      await fetchFromLink(url);
-    }
-  };
-
+      console.log(`start scraper`)
+      for (const url of urls) {
+          await fetchFromLink(url)
+      }
+      console.log('end scraper')
+      res.status(200).json(scraped_products);
+      // callback(scraped_products)
+  }
+  
   const getUrls = async () => {
-    const { data: html } = await axios(TestUrl);
-    let urls = [];
-    const $ = cheerio.load(html);
-
-    $("#main")
-      .find(".products")
-      .find(".product-category")
-      .each(async (_, elem) => {
-        const link = $(elem).find("a").attr("href");
-        urls.push(link);
-      });
-
-    return urls;
-  };
-
+      const {data: html} = await axios(TestUrl);
+      let urls = []
+      const $ = cheerio.load(html);
+  
+  
+          // console.log($('div#prod-decription').find("h1").text());
+      $('#main').find('.products').find('.product-category').each(async (_, elem) => {
+          // console.log($(elem).find(".woocommerce-loop-category__title").text());
+          // link = scrape yung link
+         
+          const link = $(elem).find("a").attr("href");
+          urls.push(link)
+          // save data in CSV here
+      })
+  
+  
+      return urls
+  }
+  
+  
   const fetchFromLink = async (link) => {
-    const {data: html} = await axios(link);
-    const $ = cheerio.load(html);
+      const {data: html} = await axios(link);
+      const $ = cheerio.load(html);
+  
+      let DataArray = [];
+      var tmpData = {
+  
+      }
+      
+      $('#prod-decription').each(async (_, elem) => {
+         
+          const productlinename = $(elem).find("h1").text();
+          const overview = $(elem).find("p").text();
+          tmpData['ProductLineName'] = productlinename
+          tmpData['Overview'] = overview
+          // DataArray.push({
+          //     ProductLineName: productlinename,
+          //     Overview: overview
+          // })
+  
+      })
+  
+  
+      $('#maintenance-tab').each(async(_, elem)=>{
+          const sku = $(elem).find("table").eq(0).find("tr").eq(0).find("td").eq(1).text();
+          const pattern = $(elem).find("table").eq(0).find("tr").eq(2).find("td").eq(1).text();
+          const fiberType = $(elem).find("table").eq(1).find("tr").eq(0).find("td").eq(1).text();
+          const faceWeight = $(elem).find("table").eq(1).find("tr").eq(1).find("td").eq(1).text();
+          const widthFeet = $(elem).find("table").eq(1).find("tr").eq(2).find("td").eq(1).text();
+          
+          tmpData['SKUs'] = sku
+          tmpData['Pattern'] = pattern
+          tmpData['WidthByFeet'] = widthFeet
+          tmpData['FiberType'] = fiberType
+          tmpData['FaceWeight'] = faceWeight
+      })
+      
+      $('.products').find('.type-product').each(async (_, elem) => {
+          const color = $(elem).find(".woocommerce-loop-product__title").text();
+          const image = $(elem).find(".woocommerce-loop-product__link").find("img").attr("src");
+          tmpData['ProductImage'] = image
+          tmpData['ColorName'] = color
+  
+          scraped_products.push(tmpData);
+      })
+  }
 
-    let DataArray = [];
-
-    $('#prod-decription').each(async (_, elem) => {
-       
-        const productlinename = $(elem).find("h1").text();
-        const overview = $(elem).find("p").text();
-        DataArray.push({
-            ProductLineName: productlinename,
-            Overview: overview
-        })
-    })
-
-    $('.products').find('.type-product').each(async (_, elem) => {
-        const color = $(elem).find(".woocommerce-loop-product__title").text();
-        const image = $(elem).find(".woocommerce-loop-product__link").find("img").attr("src");
-        DataArray.push({
-            ProductImage: image,
-            ColorName: color
-        })
-    })
-
-    $('#maintenance-tab').each(async(_, elem)=>{
-        const sku = $(elem).find("table").eq(0).find("tr").eq(0).find("td").eq(1).text();
-        const pattern = $(elem).find("table").eq(0).find("tr").eq(2).find("td").eq(1).text();
-        const fiberType = $(elem).find("table").eq(1).find("tr").eq(0).find("td").eq(1).text();
-        const faceWeight = $(elem).find("table").eq(1).find("tr").eq(1).find("td").eq(1).text();
-        const widthFeet = $(elem).find("table").eq(1).find("tr").eq(2).find("td").eq(1).text();
-        DataArray.push({
-            SKUs: sku,
-            Pattern: pattern,
-            WidthByFeet: widthFeet,
-            FiberType: fiberType,
-            FaceWeight: faceWeight,
-        })
-    })
-    const ArrayData = JSON.stringify(DataArray);
-    console.log(ArrayData);
-}
-
+  MainFunction((resdata)=>{
+  });
 });
 
 module.exports = {
